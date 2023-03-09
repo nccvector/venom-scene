c951cebe2de073bd360464d8d00f519a1c30d877
nccvector <fzn1020@gmail.com>
2023-03-09 21:10:01 +0900

    identity transform by default

--- include/Component/Transform.h
+++ include/Component/Transform.h
@@ -7,10 +7,11 @@ class Transform
 public:
   Transform()
   {
-    m_data << 1, 2, 3, 4,
-              5, 6, 7, 8,
-              9, 10, 11, 12,
-              13, 14, 15, 16;
+    // Identity by default
+    m_data << 1, 0, 0, 0,
+              0, 1, 0, 0,
+              0, 0, 1, 0,
+              0, 0, 0, 1;
   }
 
   ~Transform() {}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
+#include <iostream>
+
 #include "Scene.h"
 
 int main()
 {
   Venom::Scene::Scene* scene = new Venom::Scene::Scene();
   Venom::Scene::Node* newSceneNode = scene->createNode();
+
+  std::cout << newSceneNode->getTransform()->getData() << "\n";
 
   return 0;
 }
